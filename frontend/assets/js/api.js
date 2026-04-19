fc863378d06ed52bb77a760279e8f271da266968
Praveencity <praveenbhandari0987@gmail.com>
2026-04-19 17:57:48 +0530

    chore: update server URL for production

--- frontend/assets/js/api.js
+++ frontend/assets/js/api.js
@@ -1,4 +1,4 @@
-const SERVER_URL = "http://localhost:5000";
+const SERVER_URL = "https://land-real-estate-project-production.up.railway.app";
 const API_BASE_URL = `${SERVER_URL}/api`;
 
 const getToken = () => localStorage.getItem("lrs_token");
